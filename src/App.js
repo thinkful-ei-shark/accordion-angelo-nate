c9afcdd47755b9aa4adf7c0d8a590b9c00e4fc5c
Angelo Vazquez <angelogvazquez@gmail.com>
2020-09-30 17:36:51 -0500

    accordion commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import './App.css';
-import TheDate from './state/TheDate'
-import Counter from './state/Counter'
+import React, { Component } from 'react';
+import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun';
+import Accordion from './accordion/Accordion';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <TheDate />
-      <Counter count={123} />
-    </div>
-  );
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello World</h2>
+        <HelloWorld />
+        <h2>Bomb</h2>
+        <Bomb />
+        <h2>Roulette Gun</h2>
+        <RouletteGun bulletInChamber={6} />
+        <h2>Accordion</h2>
+        <Accordion sections={sections} />
+      </div>
+    );
+  }
 }
+
 export default App;
